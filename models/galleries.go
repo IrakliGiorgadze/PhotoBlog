e59b4c48d8a0064ed5c6ba64594dd2f7621a03fd
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-04-20 03:33:26 +0400

    add gallery service

--- models/galleries.go
+++ models/galleries.go
@@ -16,6 +16,30 @@ type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
 
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{
+			&galleryGorm{
+				db,
+			},
+		},
+	}
+}
+
+type galleryService struct {
+	GalleryDB
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+var _ GalleryDB = &galleryGorm{}
+
 type galleryGorm struct {
 	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	return gg.db.Create(gallery).Error
 }
